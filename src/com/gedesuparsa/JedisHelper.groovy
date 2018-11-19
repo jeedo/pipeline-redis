7ec6e1b92a58b736b5b22fcce98f2a6799f6a474
Gede Suparsa <gede.suparsa@frostbite.com>
2018-11-19 21:55:54 +0100

    Initial commit

---
+++ src/com/gedesuparsa/JedisHelper.groovy
@@ -0,0 +1,24 @@
+package com.gedesuparsa
+
+@Grab('redis.clients:jedis:2.1.0')
+import redis.clients.jedis.*
+
+class JedisHelper implements Serializable {
+    def script
+    def pool
+    def connstr
+
+    def JedisHelper(script) {
+        this.script = script
+        pool = new JedisPool(new JedisPoolConfig(), connstr);
+    }
+
+    def start() {
+        jedis = pool.getResource()
+        jedis.set("jobs:blah:start", new Date().getTime().toString() )
+    }
+    def end() {
+        jedis = pool.getResource()
+        jedis.set("jobs:blah:end", new Date().getTime().toString() )
+    }   
+}
