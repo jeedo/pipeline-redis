6ed7b8613ef12a168f9ba790b9d8dc1ee82603d8
Gede Suparsa <gede.suparsa@frostbite.com>
2018-11-19 22:40:21 +0100

    Removing script refs

--- vars/pushToRedis.groovy
+++ vars/pushToRedis.groovy
@@ -2,15 +2,15 @@
 import redis.clients.jedis.*
 
 @NonCPS
-def rstart(script) {
+def rstart() {
     def pool = new JedisPool(new JedisPoolConfig(), "redis");
-    jedis = pool.getResource()
+    def jedis = pool.getResource()
     jedis.set("jobs:blah:start", new Date().getTime().toString() )
 }
 
 @NonCPS
-def rend(script) {
+def rend() {
     def pool = new JedisPool(new JedisPoolConfig(), "redis");
-    jedis = pool.getResource()
+    def jedis = pool.getResource()
     jedis.set("jobs:blah:end", new Date().getTime().toString() )
 }   
